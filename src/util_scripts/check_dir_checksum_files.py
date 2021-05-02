e7595e33d254d9df3e1dc7044c7da16fde006b1f
Salman Ahmed <ssahmed532@gmail.com>
2021-05-02 23:27:27 +0500

    use subprocess.run() to calculate checksums for those directories that do not have a checksums file present in them.

--- src/util_scripts/check_dir_checksum_files.py
+++ src/util_scripts/check_dir_checksum_files.py
@@ -4,6 +4,7 @@
 
 import os
 import sys
+import subprocess
 
 SHA1_EXT = ".sha1"
 
@@ -12,14 +13,32 @@ SHA1_EXT = ".sha1"
 #   also highlight those folders where the existing SHA1 checksum file
 #   is "out of date" with respect to the contents of that folder.
 
+
+
+def calculate_checksums(dir_path):
+    program = "C:\\Windows\\cfv.bat"
+    result = subprocess.run([program, "-C", "-rr", "-t", "sha1"], cwd=dir_path)
+    print("returncode: " + str(result.returncode))
+
+
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         print('Usage: python check_dir_checksum_files.py <dirPath>', file=sys.stderr)
         sys.exit(1)
 
+    compute_checksums = False
+
+    if (len(sys.argv) == 3) and sys.argv[2].lower() == "--calculate-checksums":
+        compute_checksums = True
+        print("DEBUG: compute_checksums is True")
+
     dir_path = os.path.abspath(sys.argv[1])
 
     print("Checking all sub-directories in root dir path: " + dir_path)
+
+    dirs_without_checksums = []
+    count_dirs_with_checksums = 0
+    count_dirs_without_checksums = 0
 
     with os.scandir(dir_path) as it:
         for entry in it:
@@ -30,5 +49,18 @@ if __name__ == '__main__':
                 # print("***: " + sha1_checksum_filepath)
                 if os.path.exists(sha1_checksum_filepath):
                     print("++++++ Found SHA1 checksum file for dir: " + entry.name)
+                    count_dirs_with_checksums += 1
                 else:
                     print("WARNING: SHA1 checksum file doesn't exist for dir: " + entry.name)
+                    count_dirs_without_checksums += 1
+                    dirs_without_checksums.append(entry.path)
+    
+    print("Number of directories with checksum files: {}".format(count_dirs_with_checksums))
+    print("Number of directories without checksum files: {}".format(count_dirs_without_checksums))
+    print()
+    print(' '.join(dirs_without_checksums))
+
+    if compute_checksums and (len(dirs_without_checksums) > 0):
+        print("Computing checksums in {} directories ...".format(len(dirs_without_checksums)))
+        for dir_path in dirs_without_checksums:
+            calculate_checksums(dir_path)
