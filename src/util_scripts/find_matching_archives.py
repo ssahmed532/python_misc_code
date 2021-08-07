b6c11995a703411be8e7d831663b85c622bbdb4c
Salman Ahmed <ssahmed532@gmail.com>
2021-08-07 13:28:48 +0500

    add instructions on how to setup unrar.exe together with the Windows 10 host Cygwin installation in order to work around errors from the rarfile module "Cannot find working tool".
    add some exception handling to handle split / multi-part RAR archives.

--- src/util_scripts/find_matching_archives.py
+++ src/util_scripts/find_matching_archives.py
@@ -2,9 +2,37 @@
 # Python script to check how many RAR archive files inside a given directory
 # 'match' a specific keyword.
 #
-# required the following modules to be pip installed:
+# This script requires the following Python modules to be pip installed:
 #       rarfile
 #
+# Additional steps needed to make rarfile work on Windows:
+#       - Setup and install cygwin (latest version) 64-bit for Windows
+#       - make sure to install the following cygwin packages:
+#           gcc, make, makedepend
+#
+# rarfile needs unrar.exe to be present on the Windows path; so:
+#       - perform the following steps to compile & install unrar.exe using
+#         cygwin:
+#           (do the following steps in a cygwin shell)
+#           cd /tmp
+#           wget http://www.rarlab.com/rar/unrarsrc-5.1.7.tar.gz
+#           tar -xzvf unrarsrc-5.1.7.tar.gz
+#           cd unrar
+#           make all
+#           cp unrar.exe /bin/
+#
+#       - After the steps above, unrar.exe is going to be present in
+#         cygwin's main bin folder and can used inside a new cygwin shell
+#
+#       - Modify the Windows 10 user environment variable settings and add a
+#         new environment variable named PATH with the following value:
+#           %PATH%;C:\cygwin64\bin;
+#
+# Additional references that were helpful in resolving the unrar.exe issue:
+#       https://wjianz.wordpress.com/2014/09/28/howto-extract-rar-files-on-cygwin/
+#
+#
+
 import os
 import pathlib
 from pprint import pprint as pprint
@@ -45,10 +73,14 @@ def match_by_contents(filename, keyword):
     """
     matching_files = []
 
-    with rarfile.RarFile(filename) as archive:
-        for entry in archive.infolist():
-            if keyword in entry.filename:
-                matching_files.append(entry.filename)
+    try:
+        with rarfile.RarFile(filename) as archive:
+            for entry in archive.infolist():
+                if keyword in entry.filename:
+                    matching_files.append(entry.filename)
+    except rarfile.NeedFirstVolume as nfve:
+        print(f'ERROR: unable to check a split / multi-volume RAR file: {filename}', file=sys.stderr)
+        print(nfve)
 
     return matching_files
 
@@ -62,15 +94,13 @@ if __name__ == "__main__":
     keyword = sys.argv[2]
 
     files_list = []
-
-    beginTime = timer()
-
     startTime = timer()
     files_list = fileutils.getAllFilesRecursive(dir_path)
     endTime = timer()
     elapsed_time = round(endTime - startTime, 5)
     print(f'{len(files_list)} files found in {elapsed_time} seconds')
 
+    # dict[filename] -> [matching file contents from inside the archive]
     matched_files = {}
 
     for file in files_list:
