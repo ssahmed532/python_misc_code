152f21e6f14b885412403072d7040a5542cf4b99
Salman Ahmed <ssahmed532@gmail.com>
2021-08-31 23:31:01 +0500

    refactor code and move into a Class. add elapsed time logging.

--- src/aws/s3/upload_dir_contents_to_S3_bucket.py
+++ src/aws/s3/upload_dir_contents_to_S3_bucket.py
@@ -3,13 +3,14 @@ import os
 import sys
 import hashlib
 
+from datetime import timedelta
+from timeit import default_timer as timer
 from tqdm import tqdm
 from botocore.retries import bucket
 
 import s3_utils
 
 # TODO
-#   - convert this into a Class
 #   - integrate the argparse module
 #   - integrate the Python std logging module, and log all output to
 #     an ondisk logfile
@@ -23,7 +24,8 @@ import s3_utils
 #     only upload those files in the root of the specified directory.
 #   - add an option to skip (cryptographic) hash generation
 #   - add an option to only upload a certain type of files (e.g. only PDFs, only .txt files, etc)
-#   - add total elapsed time logging
+#   - [DONE] convert this into a Class
+#   - [DONE] add total elapsed time logging
 #
 
 
@@ -39,69 +41,113 @@ def get_crypto_hash(file_path: str) -> str:
     return file_hash
 
 
-def _upload_file_to_s3_bucket(s3_resource, file_path: str, bucket_name: str, calc_hash: bool) -> bool:
-    success = True
+class S3FileUploader:
+    """A utility class to upload files to an existing S3 Bucket
+    """
 
-    real_path = os.path.realpath(file_path)
-    filename = os.path.basename(real_path)
-
-    s3_resource.Bucket(bucket_name).upload_file(Filename=real_path, Key=filename)
-
-    if calc_hash:
-        file_hash = get_crypto_hash(real_path)
-
-        # TODO: the following line should only be printed when in verbose mode
-        #print(f'[{filename}] -> {file_hash}')
-
-        file_hash_filename = real_path + ".hash"
-        try:
-            with open(file_hash_filename, "w+") as f:
-                f.write("{} {}\n".format(filename, file_hash))
-
-            s3_resource.Bucket(bucket_name).upload_file(Filename=real_path + ".hash", Key=filename + ".hash")
-        except:
-            print(f'ERROR: unable to write integrity hash to file {file_hash_filename}')
-
-        # TODO: move this file deletion step to the above try ... except block
-        try:
-            os.remove(file_hash_filename)
-        except:
-            print(f'ERROR: unable to delete crypto hash file {file_hash_filename}')
-
-    return success
+    def __init__(self, bucket_name: str) -> None:
+        self.bucket_name = bucket_name
+        self.s3_resource = None
 
 
-def upload_dir_contents_to_s3_bucket(s3_resource, dir_path: str, bucket_name: str) -> int:
-    files_uploaded = 0
+    def initialize(self) -> None:
+        resource = boto3.resource('s3')
 
-    # Iterate over all files in the specified directory and upload them
-    # to this S3 bucket
-    # TODO: replace os.walk() with os.scandir()
-    for subdir, dirs, files in os.walk(dir_path):
-        for filename in (progress_bar := tqdm(files, desc='Uploading files')):
-            file_path = subdir + os.sep + filename
-            _upload_file_to_s3_bucket(s3_resource, file_path, bucket_name, True)
-            files_uploaded += 1
-            progress_bar.write(filename)
-
-    return files_uploaded
+        if not s3_utils.check_bucket(resource, self.bucket_name):
+            # TODO: raise an appropriate Exception here
+            print(f'ERROR: cannot upload file(s) to non-existent S3 bucket ({self.bucket_name})')
+        else:
+            self.s3_resource = resource
 
 
-def upload_file_to_s3_bucket(s3_resource, file_path: str, bucket_name: str) -> None:
-    _upload_file_to_s3_bucket(s3_resource, file_path, bucket_name, True)
+    def _upload_file_to_s3_bucket(self, file_path: str, calc_hash: bool) -> bool:
+        """Upload specified file to S3 bucket (internal helper)
+
+        Args:
+            file_path (str): full path to file to be uploaded
+            calc_hash (bool): if True, compute the integrity hash for the file
+
+        Returns:
+            bool: True if file was uploaded successfully
+        """
+        success = True
+
+        real_path = os.path.realpath(file_path)
+        filename = os.path.basename(real_path)
+
+        self.s3_resource.Bucket(self.bucket_name).upload_file(Filename=real_path, Key=filename)
+
+        if calc_hash:
+            file_hash = get_crypto_hash(real_path)
+
+            # TODO: the following line should only be printed when in verbose mode
+            #print(f'[{filename}] -> {file_hash}')
+
+            file_hash_filename = real_path + ".hash"
+            try:
+                with open(file_hash_filename, "w+") as f:
+                    f.write("{} {}\n".format(filename, file_hash))
+
+                self.s3_resource.Bucket(self.bucket_name).upload_file(Filename=real_path + ".hash", Key=filename + ".hash")
+            except:
+                print(f'ERROR: unable to write integrity hash to file {file_hash_filename}')
+
+            # TODO: move this file deletion step to the above try ... except block
+            try:
+                os.remove(file_hash_filename)
+            except:
+                print(f'ERROR: unable to delete crypto hash file {file_hash_filename}')
+
+        return success
+
+
+    def upload_dir_contents(self, dir_path: str) -> int:
+        """Upload all of the files contained in a directory to the S3 bucket
+
+        Args:
+            dir_path (str): full path to the directory
+
+        Returns:
+            int: number of files successfully uploaded
+        """
+        files_uploaded = 0
+
+        # Iterate over all files in the specified directory and upload
+        # them to this S3 bucket
+        # TODO: replace os.walk() with os.scandir()
+        for subdir, dirs, files in os.walk(dir_path):
+            for filename in (progress_bar := tqdm(files, desc='Uploading files')):
+                file_path = subdir + os.sep + filename
+                self._upload_file_to_s3_bucket(file_path, True)
+                files_uploaded += 1
+                progress_bar.write(filename)
+
+        return files_uploaded
+
+
+    def upload_file(self, file_path: str) -> None:
+        """Upload a file to the S3 bucket
+
+        Args:
+            file_path (str): full path to the file
+        """
+        self._upload_file_to_s3_bucket(file_path, True)
 
 
 def main(dir_path: str, s3_bucket_name: str, is_file: bool) -> None:
-    s3_resource = boto3.resource('s3')
+    file_uploader = S3FileUploader(s3_bucket_name)
+    file_uploader.initialize()
 
-    if not s3_utils.check_bucket(s3_resource, s3_bucket_name):
-        print(f'ERROR: cannot upload file(s) to non-existent S3 bucket ({s3_bucket_name})')
+    start = timer()
+    if is_file:
+        file_uploader.upload_file(dir_path)
     else:
-        if is_file:
-            upload_file_to_s3_bucket(s3_resource, dir_path, s3_bucket_name)
-        else:
-            files_uploaded = upload_dir_contents_to_s3_bucket(s3_resource, dir_path, s3_bucket_name)
-            print(f'{files_uploaded} files uploaded successfully')
+        files_uploaded = file_uploader.upload_dir_contents(dir_path)
+        print(f'{files_uploaded} files uploaded successfully')
+    end = timer()
+
+    elapsed_time = round(end - start, 3)
+    print(f'Elapsed time: {elapsed_time} seconds')
 
 
 if __name__ == "__main__":
