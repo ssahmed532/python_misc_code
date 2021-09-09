5c016c65b7720295b301efa92f39354d80959202
Salman Ahmed <ssahmed532@gmail.com>
2021-09-09 19:49:41 +0500

    properly scrape book details from between nested tags.

--- src/webscraping/check_packt_free_ebook.py
+++ src/webscraping/check_packt_free_ebook.py
@@ -8,38 +8,44 @@ PACKT_FREE_LEARNING_URL = r'https://www.packtpub.com/free-learning'
 
 def main() -> None:
     response = requests.get(PACKT_FREE_LEARNING_URL)
-    print(response.status_code)
-    print(response.text)
-    
-    book_title = ""
-    author = ""
-    publication_date = ""
-    page_count = ""
-    book_description = ""
-    
+    #print(response.status_code)
+    #print(response.text)
+
+    book_title = "Not available"
+    author = "Not available"
+    publication_date = "Not available"
+    page_count = (-1)
+    book_description = "Not available"
+
     soup = BeautifulSoup(response.content, "html.parser")
-    print(f'Title: {soup.title.text}')   
-    
-    first_h3 = soup.select('h3')[0].text
-    print(first_h3)
-    book_title = first_h3
-    
+    print(f'Title: {soup.title.text}')
+
+    elements = soup.find_all('h3', class_='product-info__title')
+    if elements:
+        book_title = elements[0].text.strip()
+
     elements = soup.find_all('span', class_='product-info__author free_learning__author')
     for element in elements:
         author = element.text.strip()
 
-    elements = soup.find_all('span', class_='free_learning__product_pages_date')
-    for element in elements:
-        publication_date = element.text.strip()
-        
-    elements = soup.find_all('span', class_='free_learning__product_pages')
-    for element in elements:
-        page_count = element.text.strip()
-        
-    elements = soup.find_all('span', class_='free_learning__product_description')
-    for element in elements:
-        book_description = element.text.strip()
-    
+    elements = soup.find_all('div', class_='free_learning__product_pages_date')
+    if elements:
+        results = elements[0].find_all_next('span')
+        if results:
+            publication_date = results[0].text.split(':')[1].strip()
+
+    elements = soup.find_all('div', class_='free_learning__product_pages')
+    if elements:
+        results = elements[0].find_all_next('span')
+        if results:
+            page_count = int(results[0].text.split()[1])
+
+    elements = soup.find_all('div', class_='free_learning__product_description')
+    if elements:
+        results = elements[0].find_all_next('span')
+        if results:
+            book_description = results[0].text.strip()
+
     print('PACKT Free eBook details are:')
     print(f'\tTitle:            {book_title}')
     print(f'\tAuthor:           {author}')
