cfcf9245814828b5c06c896de0faa40d860cce0d
Salman Ahmed <ssahmed532@gmail.com>
2021-09-27 23:36:51 +0500

    add support for verifying the file integrity hashes inside each .hash file.

--- src/aws/s3/download_S3_bucket.py
+++ src/aws/s3/download_S3_bucket.py
@@ -1,11 +1,28 @@
 import boto3
 import os
 import sys
+
+import hash_utils
 import s3_utils
 
 from datetime import timedelta
 from timeit import default_timer as timer
 from tqdm import tqdm
+
+
+# TODO
+#   - integrate the argparse module
+#   - integrate the Python std logging module, and log all output to
+#     an ondisk logfile
+#   - by default, this script should download the contents of the bucket
+#     into a local dir with the same name as the S3 bucket
+#   - if a -d <download dir/path> option is specified, then the contents of the
+#     S3 bucket should be downloaded into that specified dir/path
+#   - modify this script such that the integrity hash files are an internal
+#     implementation detail and nothing about them is visible except for when
+#     file integrity verification fails
+#   - add a verbose mode
+#
 
 
 class S3FileDownloader:
@@ -15,16 +32,20 @@ class S3FileDownloader:
     def __init__(self, bucket_name: str) -> None:
         self.bucket_name = bucket_name
         self.s3_resource = None
+        self.hash_files = []
 
-
-    def initialize(self) -> None:
+    def initialize(self) -> bool:
         resource = boto3.resource('s3')
 
-        if not s3_utils.check_bucket(resource, self.bucket_name):
+        bucket_exists = s3_utils.check_bucket(resource, self.bucket_name)
+
+        if not bucket_exists:
             # TODO: raise an appropriate Exception here
             print(f'ERROR: cannot download files from a non-existent S3 bucket ({self.bucket_name})')
         else:
             self.s3_resource = resource
+
+        return bucket_exists
 
     def download_all_files(self) -> int:
         files_downloaded = 0
@@ -38,13 +59,28 @@ class S3FileDownloader:
             self.s3_resource.Bucket(self.bucket_name).download_file(file, full_file_path)
             files_downloaded += 1
             progress_bar.write(full_file_path)
+            split_tup = os.path.splitext(full_file_path)
+            if split_tup[1] == '.hash':
+                self.hash_files.append(full_file_path)
 
         return files_downloaded
+
+    def verify_hashes(self) -> int:
+        print(f'Verifying {len(self.hash_files)} hash files ...')
+        for hash_filename in self.hash_files:
+            verified = hash_utils.verify_integrity_hash_file(hash_filename)
+            if verified:
+                print(f'Integrity hash file {hash_filename} is OK')
+                os.remove(hash_filename)
 
 
 def main(s3_bucket_name: str) -> None:
     file_downloader = S3FileDownloader(s3_bucket_name)
-    file_downloader.initialize()
+    init_status = file_downloader.initialize()
+
+    if not init_status:
+        print(f'ERROR: failed to initialize for downloading bucket contents')
+        sys.exit(2)
 
     start = timer()
     count = file_downloader.download_all_files()
@@ -52,6 +88,8 @@ def main(s3_bucket_name: str) -> None:
 
     elapsed_time = round(end - start, 3)
     print(f'Downloaded {count} files in time: {elapsed_time} seconds')
+
+    file_downloader.verify_hashes()
 
 
 if __name__ == "__main__":
