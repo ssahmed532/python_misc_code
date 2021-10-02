6af9925bc30efaade7db88c992cf1cd4d562acf2
Salman Ahmed <ssahmed532@gmail.com>
2021-10-02 14:48:13 +0500

    gracefully handle the case when the integrity hash file is malformed or corrupted or has information in the wrong format.

--- src/aws/s3/hash_utils.py
+++ src/aws/s3/hash_utils.py
@@ -2,6 +2,15 @@ import hashlib
 import os
 
 
+# TODO:
+#   - change the separator used in the integrity hash file from ':' to
+#     '::' to make it entirely unlikely that it could be part of a valid
+#     filename
+#
+
+HASHFILE_PART_SEP = ":"
+
+
 def get_hash(filepath: str) -> str:
     with open(filepath, "rb") as f:
         hash_algo = hashlib.blake2b()
@@ -23,7 +32,7 @@ def create_integrity_hash_file(filepath: str) -> str:
 
     try:
         with open(hash_filepath, "w+") as f:
-            f.write(f'{filename}:{file_hash}')
+            f.write(f'{filename}{HASHFILE_PART_SEP}{file_hash}')
 
         return hash_filepath
     except:
@@ -34,9 +43,14 @@ def create_integrity_hash_file(filepath: str) -> str:
 def verify_integrity_hash_file(hash_filepath: str) -> bool:
     with open(hash_filepath, "r") as f:
         line = f.readline()
-        parts = line.split(':')
+        parts = line.split(HASHFILE_PART_SEP)
+
+        if len(parts) != 2:
+            print(f'ERROR: unable to parse malformed integrity hash file {hash_filepath}')
+            return False
 
         filepath = os.path.join(os.path.dirname(hash_filepath), parts[0])
+
         hash_value = parts[1]
 
         return verify_hash(filepath, hash_value)
