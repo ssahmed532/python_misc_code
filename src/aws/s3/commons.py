32e1f87e6f21751dd016e6343de0202df06323ec
Salman Ahmed <ssahmed532@gmail.com>
2021-10-03 23:13:29 +0500

    add a few AWS regions - which will be used in upcoming changes/enhancements in these scripts.

--- src/aws/s3/commons.py
+++ src/aws/s3/commons.py
@@ -5,3 +5,12 @@ class NonExistentS3BucketError(Exception):
         self.bucket_name = bucket_name
         self.message = 'S3 Bucket does not exist'
         super().__init__(self.message)
+
+
+class AwsRegions:
+    US_EAST1 = 'us-east-1'
+    US_EAST2 = 'us-east-2'
+    US_WEST1 = 'us-west-1'
+    US_WEST2 = 'us-west-2'
+    MIDDLE_EAST1 = 'me-south-1'
+    SOUTH_AMERICA1 = 'sa-east-1'
