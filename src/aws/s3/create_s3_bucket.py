3df53d872e29e12fb757d932a23e91aae7e05254
Salman Ahmed <ssahmed532@gmail.com>
2021-10-03 23:15:31 +0500

    code refactoring & cleanup to make this S3 Bucket creation method more generic, and also to parameterize the region in which the S3 bucket has to be created.

--- src/aws/s3/create_s3_bucket.py
+++ src/aws/s3/create_s3_bucket.py
@@ -3,6 +3,9 @@ import uuid
 import pprint
 import sys
 
+from botocore.exceptions import ClientError
+import commons
+
 
 # TODO:
 #   - integrate the argparse module
@@ -13,6 +16,7 @@ import sys
 #     (or user-specified) prefix to an existing directory name. This will
 #     be helpful when uploading a whole directory to a new bucket that will
 #     be automatically created prior to uploading the directory.
+#   - add detailed logging via the Python standard logging module
 #
 
 
@@ -25,60 +29,70 @@ def get_new_bucket_name(bucket_prefix):
     return '-'.join([bucket_prefix, str(uuid.uuid4())])
 
 
-def create_bucket(bucket_name: str, s3_resource):
-    session = boto3.session.Session()
-    current_region = session.region_name
-    #current_region = 'me-south-1'
+def create_bucket(bucket_name: str, region=None) -> bool:
+    assert (len(bucket_name) > 0)
 
-    if not bucket_name:
-        # if the bucket name is not specified then auto-generate one
-        bucket_name = get_new_bucket_name(DEFAULT_BUCKET_PREFIX)
-        print(f'Auto-generated bucket name is: \"{bucket_name}\"')
+    s3_client = None
 
-    print(f'Creating new bucket \"{bucket_name}\" in region \"{current_region}\"')
-
-    s3_connection = s3_resource.meta.client
-
-    if current_region == 'us-east-1':
-        # this has to be done due to a long standing issue in the boto3 library
-        #   https://github.com/boto/boto3/issues/125
-        bucket_response = s3_connection.create_bucket(Bucket=bucket_name)
+    if region:
+        s3_client = boto3.client('s3', region_name=region)
     else:
-        bucket_response = s3_connection.create_bucket(
+        s3_client = boto3.client('s3')
+
+    try:
+        if region:
+            print(f'Creating new bucket \"{bucket_name}\" in region \"{region}\"')
+            location = {'LocationConstraint': region}
+            response = s3_client.create_bucket(
+                Bucket=bucket_name,
+                CreateBucketConfiguration=location
+                )
+        else:
+            response = s3_client.create_bucket(Bucket=bucket_name)
+    except ClientError as e:
+        print(f'S3 client error occurred while trying to create bucket: {e.response}')
+        return False
+
+    try:
+        # enable Server-side Encryption at the Bucket level
+        # reference:
+        #   https://stackoverflow.com/questions/59218289/s3-default-server-side-encryption-on-large-number-of-buckets-using-python-boto3
+        response = s3_client.put_bucket_encryption(
             Bucket=bucket_name,
-            CreateBucketConfiguration={'LocationConstraint': current_region})
+            ServerSideEncryptionConfiguration={
+                'Rules': [
+                    {
+                        'ApplyServerSideEncryptionByDefault': {'SSEAlgorithm': 'AES256'}
+                    },
+                ]
+            })
 
-    # enable Server-side Encryption at the Bucket level
-    # reference:
-    #   https://stackoverflow.com/questions/59218289/s3-default-server-side-encryption-on-large-number-of-buckets-using-python-boto3
-    response = s3_connection.put_bucket_encryption(
-        Bucket=bucket_name,
-        ServerSideEncryptionConfiguration={
-            'Rules': [
-                {
-                    'ApplyServerSideEncryptionByDefault': {'SSEAlgorithm': 'AES256'}
-                },
-            ]
-        })
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            print(f'Server-side Encryption successfully enabled for bucket {bucket_name}')
+        else:
+            print(f'WARNING: failed to enable Server-side Encryption for bucket {bucket_name}')
+            pprint.pprint(response, width=1)
+    except ClientError as e:
+        print(f'S3 client error occurred while trying to enable encryption: {e.response}')
+        return False
 
-    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-        print(f'Server-side Encryption successfully enabled for bucket {bucket_name}')
-    else:
-        print(f'WARNING: failed to enable Server-side Encryption for bucket {bucket_name}')
-        pprint.pprint(response, width=1)
-
-    return bucket_name, bucket_response
+    return True
 
 
 def main(s3_bucket_name: str) -> None:
     s3_resource = boto3.resource('s3')
 
-    s3_bucket_name, create_bucket_response = create_bucket(s3_bucket_name, s3_resource)
+    if not s3_bucket_name:
+        # if the bucket name is not specified then auto-generate one
+        s3_bucket_name = get_new_bucket_name(DEFAULT_BUCKET_PREFIX)
+        print(f'Auto-generated bucket name is: \"{s3_bucket_name}\"')
 
-    if create_bucket_response['ResponseMetadata']['HTTPStatusCode'] == 200:
+    #create_bucket_status = create_bucket(s3_bucket_name, commons.AwsRegions.MIDDLE_EAST1)
+    create_bucket_status = create_bucket(s3_bucket_name)
+
+    if create_bucket_status:
         print('S3 bucket created successfully')
-
-    print("Name of newly created S3 bucket: " + s3_bucket_name)
+        print(f"Name of newly created S3 bucket is {s3_bucket_name}")
 
     # TODO
     #   add a --verbose flag that prints out the full create_bucket response
