bc6e17fa24022d71446e80872a6666e6fa2464a1
Salman Ahmed <ssahmed532@gmail.com>
2021-10-07 18:59:28 +0500

    - integrate argparse module - add a user confirmation/prompt and only proceed with bucket deletion if: 	- user has entered Y or Yes 	- or user has chosen to ignore the confirmation/prompt via the -y CLI argument

--- src/aws/s3/delete_bucket.py
+++ src/aws/s3/delete_bucket.py
@@ -1,3 +1,4 @@
+import argparse
 import boto3
 import pprint
 import sys
@@ -8,7 +9,6 @@ import s3_utils
 from botocore.exceptions import ClientError
 
 # TODO
-#   - integrate the argparse module
 #   - add an interactive prompt that checks whether the user really
 #     wants to delete the specified bucket
 #   - handle non-empty buckets by prompting the user once more and then
@@ -17,12 +17,24 @@ from botocore.exceptions import ClientError
 #
 
 
-def delete_bucket(bucket_name):
+def delete_bucket(bucket_name: str, verbose: bool) -> bool:
+    if verbose:
+        print(f'boto3 library version is {boto3.__version__}')
+        print(f'Current region is {boto3.Session().region_name}')
+        print()
+
     bucket_location = s3_utils.get_bucket_location(bucket_name)
 
     try:
+        print(f'Deleting S3 bucket {bucket_name} in location {bucket_location} ...')
+
         s3_client = boto3.client('s3', region_name=bucket_location)
         response = s3_client.delete_bucket(Bucket=bucket_name)
+
+        if verbose:
+            print('delete_bucket() response:')
+            pprint.pprint(response)
+            print()
 
         if response['ResponseMetadata']['HTTPStatusCode'] == 204:
             print(f'S3 bucket {bucket_name} successfully deleted')
@@ -34,9 +46,48 @@ def delete_bucket(bucket_name):
         return False
 
 
-if __name__ == "__main__":
-    if (len(sys.argv) < 2):
-        print('Usage: {} <S3 bucket name>'.format(sys.argv[0]), file=sys.stderr)
-        sys.exit(1)
+def user_confirm() -> bool:
+    answer = ""
 
-    delete_bucket(bucket_name=sys.argv[1])
+    while answer not in ["y", "yes", "n", "no"]:
+        answer = input("Are you sure you want to delete the S3 bucket? [Y/N] ").lower()
+
+    return (answer == "y") or (answer == "yes")
+
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser(
+        description='Script to delete an existing S3 Bucket')
+
+    arg_parser.add_argument(
+        "-v",
+        "--verbose",
+        required=False,
+        action="store_true",
+        help="display verbose output"
+        )
+
+    arg_parser.add_argument(
+        's3_bucket_name',
+        type=str,
+        help='name of the S3 Bucket'
+        )
+
+    arg_parser.add_argument(
+        "-y",
+        "--yes",
+        required=False,
+        action="store_true",
+        help="assume Yes to all confirmation prompts"
+        )
+
+    args = arg_parser.parse_args()
+
+    no_user_prompt = args.yes
+
+    if no_user_prompt:
+        print(f'WARNING: proceeding with deleting S3 bucket {args.s3_bucket_name} without prompt/confirmation ...')
+        delete_bucket(bucket_name=args.s3_bucket_name, verbose=args.verbose)
+    else:
+        if user_confirm():
+            delete_bucket(bucket_name=args.s3_bucket_name, verbose=args.verbose)
